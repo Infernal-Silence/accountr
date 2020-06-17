edcc0d8ae10639d47b8e03f540bf49788aca74cd
Infernal-Silence <linka-pink@mail.ru>
2020-06-17 22:56:10 +0500

    fix: Поправила скрипт создания базы данных

--- database_create.py
+++ database_create.py
@@ -1,34 +1,41 @@
+import os
 import sqlite3
-from database import db
+from dotenv import load_dotenv
 
-with db.connection as con:
-	cursor = con.cursor()
-	cursor.executescript("""
-		CREATE TABLE IF NOT EXISTS types ( 
-		  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		  name TEXT NOT NULL
-		);
-		CREATE TABLE IF NOT EXISTS users ( 
-		  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		  first_name TEXT NOT NULL,
-		  last_name TEXT NOT NULL,
-		  email TEXT NOT NULL UNIQUE,
-		  password TEXT NOT NULL
-		);
-		CREATE TABLE IF NOT EXISTS categories ( 
-		  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		  name TEXT NOT NULL,
-		  parent_id INTEGER NOT NULL,
-		  user_id INTEGER NOT NULL REFERENCES users(id)
-		);
-		CREATE TABLE IF NOT EXISTS operations ( 
-		  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		  type_id INTEGER NOT NULL REFERENCES types(id),
-		  amount INTEGER NOT NULL,
-		  operation_date DATETIME NOT NULL,
-		  user_id INTEGER NOT NULL REFERENCES users(id),
-		  description TEXT NOT NULL,
-		  created_date DATETIME NOT NULL,
-		  category_id INTEGER NOT NULL REFERENCES categories(id)
-		);	
-		""")
+
+load_dotenv()
+db_connection = os.getenv('DB_CONNECTION')
+
+
+with sqlite3.connect(db_connection) as connection:
+    connection.executescript("""
+        CREATE TABLE IF NOT EXISTS types ( 
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS users ( 
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS categories ( 
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL REFERENCES users(id),
+            name TEXT NOT NULL,
+            parent_id INTEGER NOT NULL,
+            UNIQUE (user_id, name)
+        );
+        CREATE TABLE IF NOT EXISTS operations ( 
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL REFERENCES users(id),
+            type_id INTEGER NOT NULL REFERENCES types(id),
+            category_id INTEGER NOT NULL REFERENCES categories(id),
+            amount INTEGER NOT NULL,
+            description TEXT NOT NULL,
+            operation_date DATETIME NOT NULL,
+            created_date DATETIME NOT NULL
+        );    
+    """)
+    connection.commit()
