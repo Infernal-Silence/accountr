0d81a9548d64166a00bdc3c4decab05471bb2d2e
AlexandrGrents <alx.grents@gmail.com>
2020-06-18 12:10:15 +0500

    feat: зарегистрировал blueprint в app.py

--- src/accountr/app.py
+++ src/accountr/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 
-from .blueprints import auth_bp
-from .database import db
+from accountr.blueprints import auth_bp, users_bp
+from accountr.database import db
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('accountr.config.Config')
+    app.config.from_object('config.Config')
     db.init_app(app)
     app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(users_bp, url_prefix = '/users')
     return app
