1e1dc45dcbd76f2c31bdcea029c2c359ce27de30
AlexandrGrents <alx.grents@gmail.com>
2020-06-18 11:28:22 +0500

    fix: исправил код ошибки с 403 на 401

--- src/accountr/auth.py
+++ src/accountr/auth.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from http import HTTPStatus
 
 from flask import session
 
@@ -8,21 +9,22 @@ from database import db
 def auth_required(view_func):
 	'''Декоратор проверки авторизации пользователя.
 	Добавляет параметр user_id к декорируемой функции
-	Если пользователь не авторизирован, возвращается код ошибки 403'''
+	Если пользователь не авторизирован, возвращается код ошибки 401'''
 	@wraps(view_func)
 	def wrapper(*args, **kwargs):
 		user_id = session.get('user_id')
 		if not user_id:
-			return '', 403
+			return '', HTTPStatus.UNAUTHORIZED
 		with db.connection as con:
-			cur = con.execute(
-				'SELECT users.id '
-				'FROM users '
-				'WHERE id = ?',
+			cur = con.execute("""
+				SELECT users.id 
+				FROM users 
+				WHERE id = ?
+				LIMIT 1;""",
 				(user_id,),
 			)
 			user = cur.fetchone()
 		if not user:
-			return '', 403
+			return '', HTTPStatus.UNAUTHORIZED
 		return view_func(*args, **kwargs, user_id = user_id)
 	return wrapper
