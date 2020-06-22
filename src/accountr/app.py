d0d959b7b67f4006c3b4bcb7cac897d8ae53c5f7
Konstantin Novoselov <konons@gmail.com>
2020-06-22 12:39:19 +0500

    feature(operations): обработка операций - создание, чтение, изменение, удаление

--- src/accountr/app.py
+++ src/accountr/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from accountr.blueprints import auth_bp, users_bp, categories_bp
+from accountr.blueprints import auth_bp, users_bp, categories_bp, operations_bp
 from accountr.database import db
 
 
@@ -11,4 +11,5 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(users_bp, url_prefix='/users')
     app.register_blueprint(categories_bp, url_prefix='/categories')
+    app.register_blueprint(operations_bp, url_prefix='/operations')
     return app
--- src/accountr/blueprints/__init__.py
+++ src/accountr/blueprints/__init__.py
@@ -1,3 +1,5 @@
 from .auth import bp as auth_bp
 from .users import bp as users_bp
 from .categories import bp as categories_bp
+from .operations import bp as operations_bp
+
